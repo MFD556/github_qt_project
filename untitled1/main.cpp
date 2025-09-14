be1058c74a4307e3c12edfe0d928d0aa2e299722
MFD556 <15553377155@163.com>
2025-09-14 19:14:51 +0800

    222

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    qDebug()<< "hello world11112233";
+    qDebug()<< "hello world11112233444555";
 
     return a.exec();
 }
